c52b37abe31081d9b058db8a2ca5f19b5d423ebd
Monu <prankum2100@berkeley.edu>
2016-12-29 13:37:22 -0800

    got it to work with scrollspy. a little cleaner and smoother overall. no dealing with hashes anymore thank goodness.

--- scripts/index.js
+++ scripts/index.js
@@ -13,85 +13,10 @@ $(function() {
   var down;
   var listen;
 
-  var scrollToDiv = (div) => {
-    programmedScrolling = true;
-    $.scrollTo($(div), 800, {
-      onAfter: function() {
-        programmedScrolling = false;
-        listen = true;
-        mobile_listen = true;
-      }
-    });
-  };
-
-  var isScrolledIntoView = (elem) => {
-    var docViewTop = $(window).scrollTop();
-    var docViewMiddle = docViewTop + ($(window).height() / 2);
-    var elemTop = $(elem).offset().top;
-    return elemTop <= docViewMiddle;
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
   }
 
-  var changeActivePage = (newActivePage, newActiveDiv) => {
-    $("#current-nav-page").text(newActivePage);
-    $(".active").removeClass("active");
-    $(newActiveDiv).addClass("active");
-  };
-
-  var setActivePageIOS = () => {
-    var fullScreenDivs = $(".full-screen");
-    var docViewTop = $(window).scrollTop();
-    var docViewMiddle = docViewTop + ($(window).height() / 2);
-    var elemTop;
-    var elemBottom;
-
-    $(".full-screen").each( (i) => {
-      elemTop = $(fullScreenDivs[i]).offset().top;
-      elemBottom = elemTop + $(fullScreenDivs[i]).height();
-      if (elemTop <= docViewMiddle && elemBottom > docViewMiddle) {
-        $(".active").removeClass("active");
-        var id = fullScreenDivs[i].id.split("-")[1];
-        $("#" + id).addClass("active");
-        history.replaceState({}, '', ("#" + id));
-        return false;
-      }
-    });
-  };
-
-  // let activeDiv = window.location.hash;
-  // let activePage;
-  // switch (activeDiv) {
-  //   case "#about":
-  //     mobile_index = 1;
-  //     index = 1;
-  //     scrollToDiv("#page-about");
-  //     activePage = "ABOUT";
-  //     break;
-  //   case "#projects":
-  //     mobile_index = 2;
-  //     index = 2;
-  //     scrollToDiv("#page-projects");
-  //     activePage = "PROJECTS";
-  //     break;
-  //   case "#stuff":
-  //     mobile_index = 3;
-  //     index = 3;
-  //     scrollToDiv("#page-stuff");
-  //     activePage = "STUFF";
-  //     break;
-  //   case "#contact":
-  //     mobile_index = 4;
-  //     index = 4;
-  //     scrollToDiv("#page-contact");
-  //     activePage = "CONTACT";
-  //     break;
-  //   default:
-  //     mobile_index = 0;
-  //     index = 0;
-  //     scrollToDiv("#page-home");
-  //     activePage = "HOME";
-  //     activeDiv = "#home";
-  // }
-  // changeActivePage(activePage, activeDiv);
   let navOpen = false;
   
   var isMobile = {
@@ -137,7 +62,8 @@ $(function() {
 
   var closeNav = () => {
     document.getElementById("mySidenav").style.width = "0";
-    $("#current-nav-page").text(activePage);
+    console.log("AH");
+    $("#current-nav-page").text($(".sidenav a.active").text());
     $("#current-nav-page").removeClass("color-white");
     navOpen = false;
     if (isMobile.any()) {
@@ -152,6 +78,30 @@ $(function() {
       openNav();
     }
   };
+
+  var refreshIndex = (page) => {
+    switch (page) {
+      case "HOME":
+        index = 0;
+        mobile_index = 0;
+        break;
+      case "ABOUT":
+        index = 1;
+        mobile_index = 1;
+        break;
+      case "PROJECTS":
+        index = 2;
+        mobile_index = 2;
+        break;
+      case "STUFF":
+        index = 3;
+        mobile_index = 3;
+        break;
+      default:
+        index = 0;
+        mobile_index = 0;
+    }
+  }
 
   /* BEGINNING OF JQUERY/LOGIC */
 
@@ -196,43 +146,6 @@ $(function() {
     }
   }
 
-  // window.onhashchange = function(e) {
-  //   let activeDiv = window.location.hash;
-  //   switch (activeDiv) {
-  //     case "#about":
-  //       mobile_index = 1;
-  //       index = 1;
-  //       scrollToDiv("#page-about");
-  //       activePage = "ABOUT";
-  //       break;
-  //     case "#projects":
-  //       mobile_index = 2;
-  //       index = 2;
-  //       scrollToDiv("#page-projects");
-  //       activePage = "PROJECTS";
-  //       break;
-  //     case "#stuff":
-  //       mobile_index = 3;
-  //       index = 3;
-  //       scrollToDiv("#page-stuff");
-  //       activePage = "STUFF";
-  //       break;
-  //     case "#contact":
-  //       mobile_index = 4;
-  //       index = 4;
-  //       scrollToDiv("#page-contact");
-  //       activePage = "CONTACT";
-  //       break;
-  //     default:
-  //       mobile_index = 0;
-  //       index = 0;
-  //       scrollToDiv("#page-home");
-  //       activePage = "HOME";
-  //       activeDiv = "#home";
-  //   }
-  //   changeActivePage(activePage, activeDiv);
-  // }
-
   $("#logo-container img, .hover-menu").mouseover(() => {
     if (!isMobile.any()) {
       openNav();
@@ -261,59 +174,59 @@ $(function() {
     }
   });
 
-  $(".item").on('click', (event) => {
-    programmedScrolling = true;
-    if (history.pushState) {
-      if (window.location.hash == ("#" + event.target.id)) {
-        event.preventDefault();
-        history.replaceState({}, '', ("#" + event.target.id));
-      }
-    }
-    closeNav();
+  $(".sidebar-link").click((e) => {
+    let href = $(e.currentTarget).attr('href');
+    refreshIndex(href.split("-")[1].toUpperCase());
   });
 
-  $mobile_pages = $(".full-screen");
-  mobile_pagePos = 0;
-  mobile_up = 0;
-  mobile_listen = true;
+  $(".go-to-contact").click((e) => {
+    e.preventDefault();
+    $.scrollTo($("#page-contact"), 600);
+    index += 1;
+    mobile_index += 1;
+  });
 
-  if (isMobile.iOS()) {
+  // $mobile_pages = $(".full-screen");
+  // mobile_pagePos = 0;
+  // mobile_up = 0;
+  // mobile_listen = true;
 
-  } else {
-    $('html, body').swipe({
-      swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
-        event.preventDefault();
+  // if (isMobile.iOS()) {
 
-        if(!mobile_listen || navOpen) {
-          return;
-        }
+  // } else {
+  //   $('html, body').swipe({
+  //     swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+  //       event.preventDefault();
+
+  //       if(!mobile_listen || navOpen) {
+  //         return;
+  //       }
         
-        mobile_listen = false;
-        if (duration <= 100 || !["down", "up"].includes(direction)) { // not enough swipe
-          mobile_listen = true;
-          return false;
-        }
-        mobile_up = direction != "down";
-        mobile_index = Math.min(Math.max(0, mobile_up ? ++mobile_index : --mobile_index), $mobile_pages.length - 1);
-        mobile_pagePos = $mobile_pages.eq(mobile_index).offset().top;
+  //       mobile_listen = false;
+  //       if (duration <= 100 || !["down", "up"].includes(direction)) { // not enough swipe
+  //         mobile_listen = true;
+  //         return false;
+  //       }
+  //       mobile_up = direction != "down";
+  //       mobile_index = Math.min(Math.max(0, mobile_up ? ++mobile_index : --mobile_index), $mobile_pages.length - 1);
+  //       mobile_pagePos = $mobile_pages.eq(mobile_index).offset().top;
 
-        let pageId = $mobile_pages[mobile_index].id.split("-")[1];
-        $(this).stop();
-        if (window.location.hash == ("#" + pageId)) {
-          mobile_listen = true;
-          return;
-        }
-        window.location.hash = "#" + pageId;
-      }
-    });
-  }
-  
+  //       let pageId = $mobile_pages[mobile_index].id.split("-")[1];
+  //       $(this).stop();
+  //       if (window.location.hash == ("#" + pageId)) {
+  //         mobile_listen = true;
+  //         return;
+  //       }
+  //       window.location.hash = "#" + pageId;
+  //     }
+  //   });
+  // }
 
   $pages = $(".full-screen");
   pagePos = 0;
   down = 0;
   listen = true;
-  // index = 0;
+  index = 0;
 
   var ar = new Array(32, 33, 34, 35, 36, 37, 38, 39, 40);
 
@@ -326,35 +239,39 @@ $(function() {
     return true;
   });
 
-  // $('html, body').on('DOMMouseScroll mousewheel', function(e) {
-  //   e.preventDefault();
-  //   if(!listen || navOpen) {
-  //     return;
-  //   }
-    
-  //   listen = false;
-  //   if (Math.abs(e.originalEvent.deltaY) < 20) { // not enough scroll
-  //     listen = true;
-  //     return false;
-  //   }
-  //   down = e.originalEvent.deltaY > 0;
-  //   index = Math.min(Math.max(0, down ? ++index : --index), $pages.length - 1);
-  //   pagePos = $pages.eq(index).offset().top;
 
-  //   let pageId = $pages[index].id.split("-")[1];
-  //   $(this).stop();
-    // if (window.location.hash == ("#" + pageId)) {
-    //   listen = true;
-    //   return;
-    // }
-    // scrollToDiv(pageId);
-    // window.location.hash = "#" + pageId;
-  // });
+  $('html, body').on('DOMMouseScroll mousewheel', function(e) {
+    e.preventDefault();
+
+    if(!listen || navOpen) {
+      return;
+    }
+    
+    listen = false;
+    if (Math.abs(e.originalEvent.deltaY) < 20) { // not enough scroll
+      listen = true;
+      return false;
+    }
+    down = e.originalEvent.deltaY > 0;
+    index = Math.min(Math.max(0, down ? ++index : --index), $pages.length - 1);
+    pagePos = $pages.eq(index).offset().top;
+
+    let pageId = $pages[index].id;
+    $(this).stop();
+    $.scrollTo($("#" + pageId), 800, {
+      onAfter: function() {
+        listen = true;
+        mobile_listen = true;
+        if (!pageId.match(/contact/i)) {
+          $("#current-nav-page").text(pageId.split("-")[1].toUpperCase());
+        }
+      }
+    });
+  });
 
   var $toastContent = $('<span>For the smoothest experience, consider using <a target="_blank" href="https://www.google.com/chrome/">Google Chrome</a>.</span>');
   if (!isMobile.any() && !isMobile.isPad() && !(!!window.chrome && !!window.chrome.webstore)) {
     // only show toast if not mobile, iPad, or Chrome, AND user is on home page
-    console.log($(".active"));
     if (index == 0) {
       Materialize.toast($toastContent, 3000, 'rounded');
     }
@@ -375,21 +292,35 @@ $(function() {
     }
   });
 
-  window.addEventListener("orientationchange", function() {
-    if (isMobile.any()) {
-        if (window.orientation == 0) {
-        $("#thats-me").removeClass("hide").addClass("bounceIn");
-      } else {
-        $("#thats-me").removeClass("bounceIn").addClass("hide");
-      }
-    }
-  }, false);
+  $("a.next-arrow").click((e) => {
+    let href = $(e.currentTarget).attr('href');
+    $("#current-nav-page").text(href.split("-")[1].toUpperCase());
+    index += 1;
+    mobile_index += 1;
+  });
 
-  if (!isMobile.any() || isPortrait()) {
-    setTimeout( () => {
-      $("#thats-me").removeClass("hide").addClass("bounceIn");
-    }, 1000);
-  }
+  $(".arrow-color").click((e) => {
+    index = 0;
+    mobile_index = 0;
+    $("#current-nav-page").text("HOME");
+  });
+
+  // window.addEventListener("orientationchange", function() {
+  //   if (isMobile.any()) {
+  //       if (window.orientation == 0) {
+  //       $("#thats-me").removeClass("hide").addClass("bounceIn");
+  //     } else {
+  //       $("#thats-me").removeClass("bounceIn").addClass("hide");
+  //     }
+  //   }
+  // }, false);
+  
+
+  // if (!isMobile.any() || isPortrait()) {
+  //   setTimeout( () => {
+  //     $("#thats-me").removeClass("hide").addClass("bounceIn");
+  //   }, 1000);
+  // }
 
   if (isMobile.any() || isMobile.isPad()) {
     $('a#mail-to').attr('href', 'mailto:pkr2100@gmail.com');
